a57ed5dac2794ea2e9e4c721591db7508b1b20b1
CJ Danyluk <Nickdan5743@gmail.com>
2019-09-23 12:58:35 -0400

    Basic start to game project

---
+++ project1/project1.py
@@ -0,0 +1,12 @@
+# Chris Danyluk & 
+# Basics of our final game project
+
+
+def main():
+    print("Welcome to {0}!/nThis game was inspired by Adventure & Zork." +
+    "The game begins with you waking up after a rough night sleep. You rub" +
+    "your eyes and blink a few times, only to realize you are not in your" +
+    "room. As you look around, you notice that you are infact in a " +
+    "{1}.".format("INSERT NAME HERE", "Mall"))
+    curLoc = "Mall"
+    curPoints = 0

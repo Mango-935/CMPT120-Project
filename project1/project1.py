28d5fd125e4be42dfa7d29018a1a9a29a1c2283a
CJ Danyluk <Nickdan5743@gmail.com>
2019-09-24 22:09:46 -0400

    Added game functions, game is now in a playable state.

--- project1/project1.py
+++ project1/project1.py
@@ -1,15 +1,44 @@
-# Chris Danyluk & 
+# Chris Danyluk & Jeremy Gargana
 # Basics of our final game project
 
 
 def main():
-    print("Welcome to {0}!/nThis game was inspired by Adventure & Zork." +
-    "The game begins with you waking up after a rough night sleep. You rub" +
-    "your eyes and blink a few times, only to realize you are not in your" +
-    "room. As you look around, you notice that you are infact in a " +
-    "{1}.".format("INSERT NAME HERE", "Mall"))
-    curLoc = "Mall"
+    print("Welcome to Super Duper Mairo!\nThis game was inspired by Adventur" +
+          "e & Zork. The game begins with you waking up after a rough night " +
+          "sleep. You rub your eyes and blink a few times, only to realize y" +
+          "ou are not in your room. As you look around, you notice that you " +
+          "are infact in a Mall.\n")
+    curLoc = 0
     curPoints = 0
+    loc1 = "You find yourself in a dimly lit mall. The walls are bare, revea" +
+    "ling cracked bricks and sheetrock."
+    loc2 = "You find yourself in a trench. Dirt, grime, and rocks tumble int" +
+    "o the center path as you stand there."
+    loc3 = "You find yourself in an air balloon. The fabric seems strained, " +
+    "as if it could rip any moment."
+    loc4 = "You find yourself in a pitch black room. There is a single green" +
+    " and red light in the center, dangling from the city."
+    for i in range(4):
+        if (curLoc == 0):
+            print(loc1)
+        elif (curLoc == 1):
+            print(loc2)
+        elif (curLoc == 2):
+            print(loc3)
+        elif (curLoc == 3):
+            print(loc4)
+        print("Score: ", curPoints)
+        input("Please press 'Enter' to continue!")
+        curPoints += 5
+        curLoc += 1
+    print("You walk over and pull both lights at the same time, because you " +
+          "can and no one's stopping you. The green light goes dark as a doo" +
+          "r begins to open, releasing you from this game. The red light, on" +
+          " the other hand, glows brighter, and brighter. Eventually, it exp" +
+          "lodes... into confetti! You Win!\n")
+    print("Game designed and programmed by Jeremy Gargana & Chris Danyluk. A" +
+          "ll rights belong to thus and you now owe us $20 for pirating our " +
+          "life's work :)")
 
 
 main()

bcfa50c2b14cb1ec2e647749fc0de7f37b18cec7
CJ Danyluk <Nickdan5743@gmail.com>
2019-10-17 14:11:05 -0400

    Added new locations and made location variables mnemonic. Resolves #1.

--- project2/project2.py
+++ project2/project2.py
@@ -12,24 +12,34 @@ def main():
     # Gives user title and background info
     curLoc = 0  # Initializes current location
     curPoints = 0  # Initializes current score
-    loc1 = "You find yourself in a dimly lit mall. The walls are bare, "
-    loc1 += "revealing cracked bricks and sheetrock."
-    loc2 = "You find yourself in a trench. Dirt, grime, and rocks tumble "
-    loc2 += "into the center path as you stand there."
-    loc3 = "You find yourself in an air balloon. The fabric seems strained, "
-    loc3 += "as if it could rip any moment."
-    loc4 = "You find yourself in a pitch black room. There is a single green "
-    loc4 += "and red light in the center, dangling from the ceiling."
+    loc_mall = "You find yourself in a dimly lit mall. The walls are bare, "
+    loc_mall += "revealing cracked bricks and sheetrock."
+    loc_trench = "You find yourself in a trench. Dirt, grime, and rocks tumble"
+    loc_trench += " into the center path as you stand there."
+    loc_balloon = "You find yourself in an air balloon. The fabric seems "
+    loc_balloon += "strained, as if it could rip any moment."
+    loc_darkRoom = "You find yourself in a pitch black room. There is a single "
+    loc_darkRoom += "green and red light in the center, dangling from the"
+    loc_darkRoom += " ceiling."
+    loc_school = "You find yourself in an abandoned classroom of your old "
+    loc_school += "school. There are old books and backpacks laying"
+    loc_school += " about the room."
+    loc_helipad = "You find yourself on a helipad in the dead of night."
+    loc_helipad += " The wind batters against you, disturbing your focus."
     # Descriptions of each location given to the user.
-    for i in range(4):
+    for i in range(6):
         if (curLoc == 0):
-            print(loc1)
+            print(loc_mall)
         elif (curLoc == 1):
-            print(loc2)
+            print(loc_trench)
         elif (curLoc == 2):
-            print(loc3)
+            print(loc_balloon)
         elif (curLoc == 3):
-            print(loc4)
+            print(loc_helipad)
+        elif (curLoc == 4):
+            print(loc_school)
+        elif (curLoc == 5):
+            print(loc_darkRoom)
         print("Score: ", curPoints)  # Prints Score
         input("Please press 'Enter' to continue!")  # Gets the enter key input
         curPoints += 5  # Increments the score up by 5
